5a60886b276ff88b2bf04499ac616aa6fa7ae8ea
Muhamad Akbar Bin Widayat <muhamad.akbar@bfoxint.com>
2020-06-21 16:11:10 +0700

    [4.3] The Slice Type

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,36 +1,35 @@
 // fn main() {
 //     let mut s = String::from("hello");
-// 
+//
 //     s.push_str(", world!");
 //     println!("{}", s);
-// 
+//
 //     let s1 = "hello";
 //     let s1 = String::from("hello");
 //     let s2 = s1;
-//     println!("{}, world!", s1);
-// 
+//     println!("{}, world!", s1); //
 //     let s2 = s1.clone();
 //     println!("s1 = {}, s2 = {}", s1, s2);
-// 
-// 
+//
+//
 //     Ownership and Functions
 //     let s = String::from("hello");
-// 
+//
 //     takes_ownership(s);
 //     Invalid display s
 //     println!("After {}", s);
-// 
+//
 //     let x = 5;
-// 
+//
 //     makes_copy(x);
 //     println!("After {}", x);
 // }
-// 
-// 
+//
+//
 // fn takes_ownership(some_string: String) {
 //     println!("In {}", some_string);
 // }
-// 
+//
 // fn makes_copy(some_integer: i32) {
 //     println!("In {}", some_integer);
 // }
@@ -40,35 +39,35 @@
 // fn main() {
 //     let s1 = gives_ownership();         // gives_ownership moves its return
 //                                         // value into s1
-// 
+//
 //     println!("s1 {}", s1);
-// 
+//
 //     let s2 = String::from("hello");     // s2 comes into scope
 //     println!("s2 {}", s2);
-// 
+//
 //     let s3 = takes_and_gives_back(s2);  // s2 is moved into
 //                                         // takes_and_gives_back, which also
 //                                         // moves its return value into s3
 //     println!("s3 {}", s3);
-// 
+//
 // } // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
 //   // moved, so nothing happens. s1 goes out of scope and is dropped.
-// 
+//
 // fn gives_ownership() -> String {             // gives_ownership will move its
 //                                              // return value into the function
 //                                              // that calls it
-// 
+//
 //     let some_string = String::from("hello"); // some_string comes into scope
-// 
+//
 //     some_string                              // some_string is returned and
 //                                              // moves out to the calling
 //                                              // function
 // }
-// 
+//
 // // takes_and_gives_back will take a String and return one
 // fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
 //                                                       // scope
-// 
+//
 //     a_string  // a_string is returned and moves out to the calling function
 // }
 //
@@ -76,14 +75,14 @@
 // fn main() {
 //     let s1 = String::from("hello world!");
 //     // let s1 = "hello";
-// 
+//
 //     let (s2, len) = calculate_length(s1);
 //     println!("The length of {} is {}", s2, len);
 // }
-// 
+//
 // fn calculate_length(s: String) -> (String, usize) {
 //     let len = s.len();
-// 
+//
 //     (s, len)
 // }
 
@@ -91,10 +90,10 @@
 
 // fn main() {
 //     let s1 = String::from("hello");
-// 
+//
 //     let len = calculate_length(&s1);
 // }
-// 
+//
 // fn calculate_length(s: &String) -> usize {
 //     s.len()
 // }
@@ -103,55 +102,146 @@
 
 // fn main() {
 //   let mut s = String::from("hello");
-// 
+//
 //   change(&mut s);
 // }
-// 
+//
 // fn change(some_string: &mut String) {
 //   some_string.push_str(", world");
 // }
 
 // fn main () {
 //     let mut s = String::from("hello");
-// 
+//
 //     {
 //         let r1 = &mut s;
 //         println!("{}", r1);
 //     }
-// 
+//
 //     let r2 = &mut s;
-// 
+//
 //     println!("{}", r2);
 // }
 
 // fn main() {
 //     let mut s = String::from("hello");
-// 
+//
 //     let r1 = &s; // no problem
 //     let r2 = &s; // no problem
-// 
+//
 //     println!("{} and {}", r1, r2);
 //     // r1 and r2 are no longer used after this point
-// 
+//
 //     let r3 = &mut s; // no problem
 //     println!("{}", r3);
 // }
 
 // Dangling References
 
-fn main() {
-    // let reference_to_nothing = dangle();
-    let _reference_to_nothing = no_dangle();
-}
-
+// fn main() {
+//     let reference_to_nothing = dangle();
+//     let _reference_to_nothing = no_dangle();
+// }
+//
 // fn dangle() -> &String {
 //     let s = String::from("hello");
-// 
+//
 //     &s
 // }
+//
+// fn no_dangle() -> String {
+//     let s = String::from("hello");
+//
+//     s
+// }
 
-fn no_dangle() -> String {
-    let s = String::from("hello");
+// fn main() {
+//     let mut s = String::from("hello world");
+//
+//     let _word = first_word(&s);
+//
+//     s.clear() // this empties the String, making it equal to ""
+// }
+//
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+//
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+//
+//     s.len()
+//
+// }
 
-    s
+// String Slices
+
+// fn main() {
+//     let s = String::from("hello world");
+//
+//     let _hello = &s[0..5];
+//     let _world = &s[6..11];
+//
+//     // Or remove 0 and last index
+//     let _hello = &s[..5];
+//     let _world = &s[6..];
+//
+// }
+
+// fn main() {
+//     let mut s = String::from("hello world");
+//
+//     let word = first_word(&s);
+//
+//     s.clear(); // error
+//
+//     println!("the first word is: {}", word);
+// }
+
+// fn first_word(s: &String) -> &str {
+//     let bytes = s.as_bytes();
+//
+//     for(i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return &s[0..i];
+//         }
+//     }
+//
+//     &s[..]
+// }
+
+fn main() {
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[..]);
+    println!("the first word is: {}", word);
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal);
+    println!("the first word is: {}", word);
+
+    let word = first_word(my_string_literal);
+    println!("the first word is: {}", word);
+
+    let a = [1, 2, 3, 4, 5];
+    let _slice = &a[1..3];
+    // error to print the slice
+    // println!("the element is: {}", slice);
 }
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+
